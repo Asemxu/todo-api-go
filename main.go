0512388b166bd7afa4a88857c9bbd98cca49ba99
Renzo Jimenez Alfaro <hijo72@hotmail.com>
2024-12-04 20:27:05 -0500

    First simple api iun go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong",
+		})
+	})
+
+	r.Run() // Por defecto corre en localhost:8080
+}
